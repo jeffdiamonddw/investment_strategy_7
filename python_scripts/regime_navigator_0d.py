0727bd4edfa86057e1f44a4ba455f620b001a2cc
Jeff Diamond <jeff.diamond@jdanalytics.ca>
2026-08-05 12:59:38 -0500

    changed folds to from 2008 folds_1.parquet

--- python_scripts/regime_navigator_0d.py
+++ python_scripts/regime_navigator_0d.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
     }
     
     
-    df_folds = pd.read_parquet('strategy/folds_3.parquet')
+    df_folds = pd.read_parquet('strategy/folds_1.parquet')
     
  
     
